c94171525a667488984e2ff5d86d21e8a07ec399
Danush <danushsoosai28@gmail.com>
2025-04-25 09:07:44 -0400

    Initial sharing of project

---
+++ Solution.java
@@ -0,0 +1,55 @@
+
+/**
+ * Write a description of class Solution here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Solution
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    /**
+     * Constructor for objects of class Solution
+     */
+    public Solution()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+        // Q24
+        //(a)
+        System.out.println("Oliver");
+        System.out.println("John");
+        //(b) //(c)
+        //if(System.out.println.equal){
+        //  System.out.println("equal");
+        
+        //else{
+           // System.out.println("not equal");
+        
+        
+    }
+    
+    //Q25
+    private double temperature(){
+        //ArrayList<> = new temperature;
+        //temperature = ("33.5");
+        //temperature = ("35.5");
+        //temperature = ("40.5");
+        //for (temperature < 37.5){
+        //System.out.println("max temp")
+        return 0;
+        }
+    }
+
